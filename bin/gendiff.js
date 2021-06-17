ed7906158796bad466223c4bba674f591d218d51
Mazitov Dmitry <mazitov.dima@mail.ru>
2021-06-17 23:59:49 +0300

    Start project, add simple diff function

---
+++ bin/gendiff.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import gendiff from "../src/gendiff.js";
+
+const program = new Command();
+
+program
+    .version("0.0.1")
+    .arguments('<filepath1> <filepath2>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .action(gendiff)
+
+program.parse(process.argv);
+
---
+++ src/gendiff.js
@@ -0,0 +1,36 @@
+import { readFileSync } from 'fs';
+import _ from 'lodash';
+import path from 'path';
+
+const gendiff = (path1, path2) => {
+    const [obj1, obj2] = [path1, path2]
+        .map((pathFile) => readFileSync(path.resolve(process.cwd(), pathFile)))
+        .map(JSON.parse);
+    const uniqKeys = _.union(_.keys(obj1), _.keys(obj2));
+    const sortedKeys = _.sortBy(uniqKeys);
+    let result = '';
+
+    sortedKeys.forEach((key) => {
+        if(_.has(obj1,key) && _.has(obj2,key)){
+            if (obj1[key] === obj2[key]) {
+                result += `   ${key}: ${obj1[key]}\n`;
+            } else {
+                result += ` - ${key}: ${obj1[key]}\n`;
+                result += ` + ${key}: ${obj2[key]}\n`;
+            }
+            return;
+        }
+
+        if(!_.has(obj1,key) && _.has(obj1,key)) {
+            result += ` - ${key}: ${obj1[key]}\n`;
+        }else {
+            result += ` + ${key}: ${obj2[key]}\n`;
+        }
+
+
+    });
+
+    console.log(`{\n${result}}`);
+};
+
+export default gendiff;
