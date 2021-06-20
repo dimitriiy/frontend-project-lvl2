6424c864559a3a43d4713b77fe9c780bcb83a2e6
Mazitov Dmitry <mazitov.dima@mail.ru>
2021-06-20 20:59:50 +0300

    Fix eslint warnings

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import gendiff from "../src/gendiff.js";
+import gendiff from '../src/gendiff.js';
 
 const program = new Command();
 
 program
-    .version("0.0.1")
-    .arguments('<filepath1> <filepath2>')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format')
-    .action(gendiff)
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .action(gendiff);
 
 program.parse(process.argv);
-
