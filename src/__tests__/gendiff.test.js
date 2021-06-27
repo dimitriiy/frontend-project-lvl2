9b9d7c78f03506746f4205aaac96b48c47cba23a
Mazitov Dmitry <mazitov.dima@mail.ru>
2021-06-27 14:27:37 +0300

    Fixed lint

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -1,26 +1,23 @@
-import gendiff from '../gendiff'
-import  path from "path";
-import  fs from "fs";
+import path from 'path';
+import fs from 'fs';
+import gendiff from '../gendiff';
 
-const getFixturePath = (filename) => path.join(__dirname,"..","__fixtures__",filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe("gendiff",() => {
+describe('gendiff', () => {
+  it('successfull', () => {
+    const file1 = readFile('file1.json');
+    const file2 = readFile('file2.json');
 
-    it("successfull",() => {
-        const file1 = readFile("file1.json");
-        const file2 = readFile("file2.json");
-
-        expect(gendiff(file1,file2)).toEqual(`{
+    expect(gendiff(file1, file2)).toEqual(`{
  + follow: false
    host: hexlet.io
  + proxy: 123.234.53.22
  - timeout: 50
  + timeout: 20
  - verbose: true
-}`)
-    })
-})
-
-
+}`);
+  });
+});
