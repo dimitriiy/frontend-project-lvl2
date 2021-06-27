723c29d3eaf7588747fecab857ab8ce5f1e39849
Mazitov Dmitry <mazitov.dima@mail.ru>
2021-06-27 17:42:06 +0300

    Fixed tests

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -1,7 +1,10 @@
 import path from 'path';
 import fs from 'fs';
+import { fileURLToPath } from "url"; // the node package 'url'
+import gendiff from '../gendiff.js';
 
-import gendiff from '../gendiff';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
