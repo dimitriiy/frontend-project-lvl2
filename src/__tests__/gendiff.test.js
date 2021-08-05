56b42f6c7a5b9b2dddcf686e32ebbf6fcc6d3f9c
Mazitov Dmitry <mazitov.dima@mail.ru>
2021-08-05 21:06:01 +0300

    Add stylish

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import fs from 'fs';
-import { fileURLToPath } from "url"; // the node package 'url'
+import { fileURLToPath } from 'url'; // the node package 'url'
 import gendiff from '../gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,20 +8,83 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-
 describe('gendiff', () => {
-  it('successfull', () => {
-    const file1 = readFile('file1.json');
-    const file2 = readFile('file2.json');
+  it('json', () => {
+    const file1 = getFixturePath('file1.json');
+    const file2 = getFixturePath('file2.json');
 
+    console.log(gendiff(file1, file2))
     expect(gendiff(file1, file2)).toEqual(`{
- + follow: false
-   host: hexlet.io
- + proxy: 123.234.53.22
- - timeout: 50
- + timeout: 20
- - verbose: true
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
 }`);
   });
+//     it('yml', () => {
+//       const file1 = getFixturePath('file1.yml');
+//       const file2 = getFixturePath('file2.yml');
+//
+//       expect(gendiff(file1, file2)).toEqual(`{
+//   + follow: false
+//     host: hexlet.io
+//   + proxy: 123.234.53.22
+//   - timeout: 50
+//   + timeout: 20
+//   - verbose: true
+// }`);
+//     });
+
+  it("nested",() => {
+
+    const file1 = getFixturePath('file1.nested.json');
+    const file2 = getFixturePath('file2.nested.json');
+
+    expect(gendiff(file1,file2)).toEqual(`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`)
+  })
 });
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,30 +1,143 @@
 import _ from 'lodash';
+import yaml from 'js-yaml';
+import path from 'path';
+import fs from 'fs';
 
-const gendiff = (file1, file2) => {
-  const [obj1, obj2] = [file1, file2].map(JSON.parse);
+const STATUS = {
+  CHANGED: 'CHANGED',
+  NOT_CHANGED: 'NOT_CHANGE',
+  ADDED: 'ADDED',
+  REMOVED: 'REMOVED',
+  HAS_CHILDREN: 'HAS_CHILDREN',
+};
+
+const LEN_INTEND = 4;
+
+const parser = (filePath) => {
+  const file = fs.readFileSync(filePath, 'utf8');
+
+  if (['.yml', '.yaml'].includes(path.extname(filePath))) {
+    return yaml.load(file);
+  }
+
+  return JSON.parse(file);
+};
+
+const getASTDiff = (obj1, obj2) => {
   const uniqKeys = _.union(_.keys(obj1), _.keys(obj2));
   const sortedKeys = _.sortBy(uniqKeys);
-  let result = '';
+  const diffTree = [];
 
   sortedKeys.forEach((key) => {
-    if (_.has(obj1, key) && _.has(obj2, key)) {
-      if (obj1[key] === obj2[key]) {
-        result += `   ${key}: ${obj1[key]}\n`;
-      } else {
-        result += ` - ${key}: ${obj1[key]}\n`;
-        result += ` + ${key}: ${obj2[key]}\n`;
-      }
+    if (!_.has(obj1, key) && _.has(obj2, key)) {
+      diffTree.push({
+        key,
+        status: STATUS.ADDED,
+        value: obj2[key],
+      });
+
       return;
     }
 
-    if (!_.has(obj1, key) && _.has(obj2, key)) {
-      result += ` - ${key}: ${obj2[key]}\n`;
+    if (_.has(obj1, key) && !_.has(obj2, key)) {
+      diffTree.push({
+        key,
+        status: STATUS.REMOVED,
+        value: obj1[key],
+      });
+      return;
+    }
+
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      diffTree.push({
+        key,
+        status: STATUS.HAS_CHILDREN,
+        value: getASTDiff(obj1[key], obj2[key]),
+      });
+      return;
+    }
+
+    if (obj1[key] === obj2[key]) {
+      diffTree.push({
+        key,
+        status: STATUS.NOT_CHANGED,
+        value: obj1[key],
+      });
     } else {
-      result += ` + ${key}: ${obj1[key]}\n`;
+      diffTree.push({
+        key,
+        status: STATUS.CHANGED,
+        oldValue: obj1[key],
+        value: obj2[key],
+      });
     }
   });
 
-  return `{\n${result}}`;
+  return diffTree;
+};
+
+const printObject = (obj, offset = LEN_INTEND) => {
+  const keys = _.keys(obj);
+  const offsetString = ' '.repeat(offset);
+  const endOffset = ' '.repeat(offset - LEN_INTEND);
+  let result = '';
+
+  keys.forEach((key) => {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      result += `${offsetString}${key}: ${printObject(obj[key], offset + LEN_INTEND)}\n`;
+    } else {
+      result += `${offsetString}${key}: ${obj[key]}\n`;
+    }
+  });
+
+  return `{\n${result}${endOffset}}`;
+};
+
+const printRow = (value, offset = 0) => {
+  if (typeof value === 'object' && value !== null) {
+    return printObject(value, offset);
+  }
+
+  return value;
+};
+const stylish = (tree, offset = LEN_INTEND) => {
+  let stringifyObject = '';
+  const baseIntend = ' '.repeat(offset);
+  const diffIntend = ' '.repeat(offset - 2);
+  const closeIntend = ' '.repeat(offset - LEN_INTEND);
+
+  tree.forEach((item) => {
+    if (item.status === STATUS.HAS_CHILDREN) {
+      stringifyObject += `${baseIntend}${item.key}: ${stylish(item.value, offset + LEN_INTEND)}\n`;
+    }
+
+    if (item.status === STATUS.NOT_CHANGED) {
+      stringifyObject += `${baseIntend}${item.key}: ${item.value}\n`;
+    }
+
+    if (item.status === STATUS.CHANGED) {
+      stringifyObject += `${diffIntend}- ${item.key}: ${printRow(item.oldValue, offset + LEN_INTEND)}\n`;
+      stringifyObject += `${diffIntend}+ ${item.key}: ${printRow(item.value, offset + LEN_INTEND)}\n`;
+    }
+
+    if (item.status === STATUS.ADDED) {
+      stringifyObject += `${diffIntend}+ ${item.key}: ${printRow(item.value, offset + LEN_INTEND)}\n`;
+    }
+
+    if (item.status === STATUS.REMOVED) {
+      stringifyObject += `${diffIntend}- ${item.key}: ${printRow(item.value, offset + LEN_INTEND)}\n`;
+    }
+  });
+
+  return `{\n${stringifyObject}${closeIntend}}`;
+};
+
+const gendiff = (path1, path2) => {
+  const [obj1, obj2] = [path1, path2].map(parser);
+
+  const diffTree = getASTDiff(obj1, obj2);
+
+  return stylish(diffTree);
 };
 
 export default gendiff;
